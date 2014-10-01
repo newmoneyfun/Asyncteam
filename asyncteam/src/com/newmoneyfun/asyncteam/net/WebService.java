c019c874fe6f5698d907db0b0e96f46a14183d98
newmoneyfun <newmoneyfun@qq.com>
2014-10-01 18:49:42 +0800

    add class net/WebService.java

---
+++ asyncteam/src/com/newmoneyfun/asyncteam/net/WebService.java
@@ -0,0 +1,29 @@
+package com.newmoneyfun.asyncteam.net;
+
+/**
+ * web server
+ * @author ps
+ *
+ */
+public class WebService {
+	
+	
+	/**
+	 * 提交数据到服务器
+	 * @param method
+	 */
+	public void submitData(String method){
+		
+		submitData(method, null, null);
+	}
+	/**
+	 * 提交数据到服务器
+	 * @param method 方法名
+	 * @param args 参数
+	 * @param json json
+	 */
+	public void submitData(String method,String args,String json){
+		
+	}
+
+}
