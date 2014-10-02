59d361e6bccf80fd716ada4ebcc0d7ca58a99a65
newmoneyfun <newmoneyfun@qq.com>
2014-10-02 20:44:26 +0800

    add login function

--- asyncteam/src/com/newmoneyfun/asyncteam/fragment/LoginFragment.java
+++ asyncteam/src/com/newmoneyfun/asyncteam/fragment/LoginFragment.java
@@ -1,22 +1,26 @@
 package com.newmoneyfun.asyncteam.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.newmoneyfun.asyncteam.MainActivity;
 import com.newmoneyfun.asyncteam.R;
 
 public class LoginFragment extends Fragment{
 	
 	private EditText et_account;
 	private EditText et_password;
+	private Button btn_login;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,11 +28,17 @@ public class LoginFragment extends Fragment{
 		View view = inflater.inflate(R.layout.fragment_login, container, false); 
 		et_account=(EditText) view.findViewById(R.id.et_login_account);
 		et_password=(EditText) view.findViewById(R.id.et_login_password);
-		et_account.setOnClickListener(new OnClickListener() {
+		
+		btn_login=(Button) view.findViewById(R.id.btn_login_submit);
+		btn_login.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				
+				if(et_account.getText().toString().equals("fan")&&et_password.getText().toString().equals("fan")){
+					Intent intent=new Intent(getActivity().getApplicationContext(),MainActivity.class);
+					startActivity(intent);
+					getActivity().finish();
+				}
 			}
 		});
 		showTopLayout();
