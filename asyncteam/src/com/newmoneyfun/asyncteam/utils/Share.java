d93363f51d05c7dc009da699d7f61ef5d96b12ca
newmoneyfun <newmoneyfun@qq.com>
2014-10-06 11:02:50 +0800

    add class Share.java

---
+++ asyncteam/src/com/newmoneyfun/asyncteam/utils/Share.java
@@ -0,0 +1,27 @@
+package com.newmoneyfun.asyncteam.utils;
+
+import android.content.Context;
+import android.net.Uri;
+
+/**
+ * share msg or a pic to other
+ * @author Administrator
+ *
+ */
+public class Share {
+	
+	private Context context;
+	
+	public Share(Context context) {
+		this.context=context;
+	}
+	
+	public void shareMsg(String msg){
+		
+	}
+	
+	public void sharePicture(String msg,Uri uri){
+		
+	}
+
+}
